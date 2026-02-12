9ca9c8d019fcb984a87c52df46a8d31e880ae6d4
Michael Analitis <m.analitis3@gmail.com>
2026-02-12 00:05:28 -0600

    Add onchange to dropdown, fetch image URLs

--- main.js
+++ main.js
@@ -9,11 +9,19 @@ start()
 
 function createBreedList(breedList) {
     document.getElementById("breed").innerHTML = `
-    <select>
+    <select onchange="loadByBreed(this.value)">
         <option>Choose a dog breed</option>
         ${Object.keys(breedList).map(function (breed) {
             return `<option>${breed}</option>`
         }).join('')}
     </select>
     `
+}
+
+async function loadByBreed(breed) {
+    if (breed != "Choose a dog breed") {
+        const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+        const data = await response.json()
+        console.log(data)
+    }
 }
